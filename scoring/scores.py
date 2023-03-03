e3cdcbcc611f61cbb1e9d504aa3c12db7199b44b
rmarcille <77974381+rmarcille@users.noreply.github.com>
2023-03-03 17:46:27 +0100

    Number of sensors selection
    - Added a score spatially function to get normalized RMSE maps (Normalized by umag test)
    - In the notebook, funcitons and plots to hint the optimal number of sensors

--- scoring/scores.py
+++ scoring/scores.py
@@ -23,4 +23,17 @@ def score_reconstructed(X_pred, X_test):
     mean_umagpred = umagpred.mean(axis = 0)
     mean_umagtest = umagtest.mean(axis = 0)
     err_umean = mean_squared_error(mean_umagpred, mean_umagtest, squared = False)
+
     return err_umag, err_uv, err_umax, err_umean
+
+def score_spatially_normalized(X_pred, X_test):
+    n_inputs = int(X_test.shape[1]/2)
+
+    umagtest = np.sqrt(X_test[:, :n_inputs]**2 + X_test[: , n_inputs:]**2)
+    umagpred = np.sqrt(X_pred[:, :n_inputs]**2 + X_pred[: , n_inputs:]**2)
+
+    umagpred = (umagpred - umagtest.mean())/umagtest.std()
+    umagtest = (umagtest - umagtest.mean())/umagtest.std()
+
+    umag_map_nrmse = np.sqrt(((umagtest - umagpred)**2).mean(axis = 0))
+    return umag_map_nrmse
